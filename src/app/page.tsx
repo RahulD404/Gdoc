d1b4dd9cf3970d8a980d863f9465e4e54f0a9c68
RahulD404 <rd.dev404@gmail.com>
2024-12-03 02:52:28 +0530

    feat:add tiptap editor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 const Home = () =>{
   return(
     <div className="flex min-h-screen items-center justify-center bg-slate-500 gap-5">
       <p>GDoc</p>
       <Button>Online Editor</Button>
+      Click<Link href="/documents/123"><span className="text-fuchsia-600 underline">&nbsp;here&nbsp;</span> to go document Id:123</Link>
     </div>
   );
 }
